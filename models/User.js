921b9f47d40d4d755aa1a59a7f921c3e5f18a4e9
Siddharth Barhate <36545101+siddy555@users.noreply.github.com>
2024-05-20 19:55:16 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ var uniqueValidator = require('mongoose-unique-validator');
 var crypto = require('crypto');
 var jwt = require('jsonwebtoken');
 var secret = require('../config').secret;
-
+// test
 var UserSchema = new mongoose.Schema({
   username: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
   email: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
